58b7b91e715c008b120db58cbb252d460524b142
madhawk1296 <silvacristian1296@gmail.com>
2023-09-22 19:59:00 -0600

    track login

--- app/onboard/layout.tsx
+++ app/onboard/layout.tsx
@@ -1,8 +1,18 @@
 import Robot from "@/components/icons/Robot"
 import Navigation from "./Navigation"
 import { ReactNode } from "react"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/types/supabase";
+import { cookies } from "next/headers";
+import TrackLogin from "../account/TrackLogin";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+    const supabase = createServerComponentClient<Database>({ cookies })
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
     return (
         <div className="relative bg-gray-100 md:bg-gray-200 w-screen h-screen md:w-full min-h-screen relative flex flex-col items-center gap-14 md:py-[25px] overflow-hidden" >
             <div className="flex gap-1 items-center justify-center mt-[20px] md:mt-[0px] h-[35px] md:h-[45px]">
@@ -13,6 +23,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                 <Navigation />
                 {children}
             </div>
+            <TrackLogin user={user} />
         </div>
     )
 } 
