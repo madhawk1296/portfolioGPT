d3f7160c0d57a3c8486a73c344ae557114f38f08
madhawk1296 <silvacristian1296@gmail.com>
2023-10-13 22:01:01 -0600

    fix

--- actions/stripeCheckout.ts
+++ actions/stripeCheckout.ts
@@ -1,13 +1,11 @@
 'use server'
 
-import { redirect } from 'next/navigation';
 import Stripe from 'stripe';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { Database } from '@/types/supabase';
-import { RedirectType } from 'next/dist/client/components/redirect';
 
-export default async function stripeCheckout(formData: FormData): Promise<{error: string | null}> {
+export default async function stripeCheckout(formData: FormData): Promise<{data: string | null, error: string | null}> {
     const plan = formData.get('plan') as "pro";
     const billed = formData.get('billed') as "monthly" | "yearly"
     const billedYearly = billed == "yearly"
@@ -15,17 +13,14 @@ export default async function stripeCheckout(formData: FormData): Promise<{error
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2022-11-15' });
 
     try {
-      const supabase = createServerActionClient<Database>({ cookies })
-      const { data: { user } } = await supabase.auth.getUser()
-      const { data: subscription_tiers, error } = await supabase.from('subscription_tiers').select().eq("name", plan);
-      const { yearly_price_id, monthly_price_id} = subscription_tiers?.[0]!
+      //const supabase = createServerActionClient<Database>({ cookies })
+      //const { data: subscription_tiers, error } = await supabase.from('subscription_tiers').select().eq("name", plan);
+      //const { yearly_price_id, monthly_price_id} = subscription_tiers?.[0]!
       
-      console.log('starting...')
-
-      const { url } = await stripe.checkout.sessions.create({
+      const { url, id } = await stripe.checkout.sessions.create({
           line_items: [
             {
-              price: billedYearly ? yearly_price_id! : monthly_price_id!,
+              price: "price_1O0ZY2DZVYK8s4Zq9Eu5iT9h",
               quantity: 1,
             },
           ],
@@ -34,12 +29,8 @@ export default async function stripeCheckout(formData: FormData): Promise<{error
           cancel_url: `http://localhost:3000/cristian?canceled=true`,
       });
 
-      console.log({url})
-
-      redirect(url!, RedirectType.push);
-
+      return { data: id, error: null}
     } catch (err: any) {
-      console.log(err);
-      return {error: err.message as string}
+      return { data: null, error: err.message as string}
     }
 }
--- app/Upgrade/PanelButton.tsx
+++ app/Upgrade/PanelButton.tsx
@@ -1,8 +1,22 @@
-import stripeCheckout from "@/actions/stripeCheckout";
 import dosis from "@/fonts/dosis";
 import Link from "next/link";
+import { loadStripe } from '@stripe/stripe-js';
+import stripeCheckout from "@/actions/stripeCheckout";
 
 export default function PanelButton({ isFree }: { isFree: boolean} ) {
+    const handleSubmit = async () => {
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+        const formData = new FormData;
+        formData.append("plan", "pro")
+        formData.append("billed", "monthly")
+
+        const { data, error} = await stripeCheckout(formData);
+
+        if(data) {
+            stripe?.redirectToCheckout({ sessionId: data})
+        } 
+    }
+
     return isFree ? (
         <Link href="/Account">
             <button className={`absolute bottom-[20px] left-[50%] -translate-x-1/2 px-[15px] py-[10px] rounded-xl shadow-md hover:shadow-lg smoothe ${dosis.bold} tracking-widest text-blue-700 bg-blue-100`} >Create a page</button>    
